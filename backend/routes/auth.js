15ebfdaa8b991acb59a6abe0c5a336c03a633659
NITHIN <nithinkumarkunchala@gmail.com>
2025-03-20 09:16:27 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,8 +11,8 @@ const crypto = require("crypto");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS,
+     user: 'nithinkumarkunchala@gmail.com',
+     pass: 'meic klmt qpsu wmwb',
   },
 });
 
